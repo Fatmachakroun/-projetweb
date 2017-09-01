95ff46a4fae3fd3cd23602adbb3ff78c5169af1e
Fatmachakroun <fatma.chakroun@esprit.tn>
2017-09-02 00:44:23 +0200

    Update session.php

--- session.php
+++ session.php
@@ -4,7 +4,7 @@
    
    $user_check = $_SESSION['login_user'];
    
-   $ses_sql = mysqli_query($db,"select Email from User where Email = '$user_check' ");
+   $ses_sql = mysqli_query($conn,"select Email from User where Email = '$user_check' ");
 
    $row = mysqli_fetch_array($ses_sql,MYSQLI_ASSOC);
    
