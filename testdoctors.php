b63d41f9e4f29af636c722922fbe35a645f3f297
Fatmachakroun <fatma.chakroun@esprit.tn>
2017-09-01 20:05:15 +0100

    Update testdoctors.php

--- testdoctors.php
+++ testdoctors.php
@@ -62,7 +62,7 @@
       <div id="navbar" class="navbar-collapse collapse navbar_area">          
         <ul class="nav navbar-nav navbar-right custom_nav">
           <li><a href="index.html">Home</a></li>
-          <li class="active"><a href="doctors.html">Our Doctors</a></li>
+          <li><a href="doctors.html">Our Doctors</a></li>
           <li><a href="contact.html">Contact Us</a></li>
           <li class="dropdown">
             <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">Dropdown <span class="caret"></span></a>
