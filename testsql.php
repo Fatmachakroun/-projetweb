d788214a46d93c6966148c19aca0c7afb1a91b21
Fatmachakroun <fatma.chakroun@esprit.tn>
2017-09-01 16:52:49 +0100

    Add files via upload

---
+++ testsql.php
@@ -0,0 +1,19 @@
+<?php
+$serverName = "projetwebsrv.mysql.database.azure.com";
+$connectionOptions = array(
+    "Database" => "document",
+    "Uid" => "mysqluser@projetwebsrv",
+    "PWD" => "Fatma123"
+);
+//Establishes the connection
+$conn = sqlsrv_connect($serverName, $connectionOptions);
+$tsql= "SELECT ID, FullNameDoctor From Doctor;";
+$getResults= sqlsrv_query($conn, $tsql);
+echo ("Reading data from table" . PHP_EOL);
+if ($getResults == FALSE)
+    echo (sqlsrv_errors());
+while ($row = sqlsrv_fetch_array($getResults, SQLSRV_FETCH_ASSOC)) {
+ echo ($row['CategoryName'] . " " . $row['ProductName'] . PHP_EOL);
+}
+sqlsrv_free_stmt($getResults);
+?>
