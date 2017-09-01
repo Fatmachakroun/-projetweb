f102f7057ddb363fd90da449234cdf475d9de267
Fatmachakroun <fatma.chakroun@esprit.tn>
2017-09-02 00:19:27 +0200

    Update insc.php

--- insc.php
+++ insc.php
@@ -4,6 +4,7 @@ $username = 'mysqluser@projetwebsrv';
 $password = 'Fatma123';
 $db_name = 'document';
 
+
 //Establishes the connection
 $conn = mysqli_init();
 mysqli_real_connect($conn, $host, $username, $password, $db_name, 3306);
@@ -11,19 +12,25 @@ if (mysqli_connect_errno($conn)) {
 die('Failed to connect to MySQL: '.mysqli_connect_error());
 }
 
-$email='tomsor@tomsor.com';
-$password='tomsor123';
-$name ='tomsor';
-$gender='Male';
-$numuser='53853544';
+//Create an Insert prepared statement and run it
+$product_name = 'BrandNewProduct';
+$product_color = 'Blue';
+$product_price = 15.5;
+
+
+$Email='tomsor@tomsor.com';
+$Password='tomsor123';
+$FullName ='tomsor';
+$Gender='Male';
+$NumUser='53853544';
+
 if ($stmt = mysqli_prepare($conn, "INSERT INTO User (Email, Password, FullName, Gender, NumUser) VALUES (?, ?, ?, ?, ?)")) {
-mysqli_stmt_bind_param($stmt, 'ssd', $email, $password, $name, $gender, $numuser);
+mysqli_stmt_bind_param($stmt, 'ssd', $Email, $Password, $FullName, $Gender, $NumUser);
 mysqli_stmt_execute($stmt);
 printf("Insert: Affected %d rows\n", mysqli_stmt_affected_rows($stmt));
 mysqli_stmt_close($stmt);
 }
+
 // Close the connection
 mysqli_close($conn);
-     
-   
 ?>
