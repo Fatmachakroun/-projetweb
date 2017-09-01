da10a7e8211b5153c36f9a2c277d7c61071c9c70
Fatmachakroun <fatma.chakroun@esprit.tn>
2017-09-02 00:15:33 +0200

    Update insc.php

--- insc.php
+++ insc.php
@@ -1,5 +1,4 @@
 <?php
-
 $host = 'projetwebsrv.mysql.database.azure.com';
 $username = 'mysqluser@projetwebsrv';
 $password = 'Fatma123';
@@ -12,30 +11,19 @@ if (mysqli_connect_errno($conn)) {
 die('Failed to connect to MySQL: '.mysqli_connect_error());
 }
 
-
-
-
-
- 
-      
-      
-$email="tomsor@tomsor.com";
-$password="tomsor123";
-$name ="tomsor";
-$gender="Male";
-$numuser="53853544";
-
+$email='tomsor@tomsor.com';
+$password='tomsor123';
+$name ='tomsor';
+$gender='Male';
+$numuser='53853544';
 if ($stmt = mysqli_prepare($conn, "INSERT INTO User (Email, Password, FullName, Gender, NumUser) VALUES (?, ?, ?, ?, ?)")) {
 mysqli_stmt_bind_param($stmt, 'ssd', $email, $password, $name, $gender, $numuser);
 mysqli_stmt_execute($stmt);
 printf("Insert: Affected %d rows\n", mysqli_stmt_affected_rows($stmt));
 mysqli_stmt_close($stmt);
 }
-
 // Close the connection
 mysqli_close($conn);
      
-
    
 ?>
-
